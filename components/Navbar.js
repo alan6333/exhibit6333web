ac9f3e2c31a0960057c896c39ce908756d6a2493
alan6333 <alan6333.business@gmail.com>
2022-12-21 15:13:43 -0600

    navbar expr

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,16 +3,16 @@ import Link from 'next/link'
 
 export default () => {
     return (
-      <div className='text-center w-full z-10'>
+      <div className='text-center w-full z-10 bg-stone-500 py-6'>
         <div>
             <ul>
-                <li className='text-3xl UrbanistFont.localFont inline px-40 text-gray-600 hover:text-black'>
+                <li className='text-3xl UrbanistFont.localFont inline px-40 text-neutral-400 hover:text-gray-300 hover:font-semibold'>
                     <Link href='/'>Exhibition</Link>
                 </li>
-                <li className='text-3xl UrbanistFont.localFont inline px-40 text-gray-600 hover:text-black'>
+                <li className='text-3xl UrbanistFont.localFont inline px-40 text-neutral-400 hover:text-gray-300 hover:font-semibold'>
                     <Link href='/artist'>Artist</Link>
                 </li>
-                <li className='text-3xl UrbanistFont.localFont inline px-40 text-gray-600 hover:text-black'>
+                <li className='text-3xl UrbanistFont.localFont inline px-40 text-neutral-400 hover:text-gray-300 hover:font-semibold'>
                     <Link href='/subscribe'>Subscribe</Link>
                 </li>
             </ul>
