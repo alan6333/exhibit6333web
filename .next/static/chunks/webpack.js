f70bafbd8f5257f6e1a95d9e013456aa58b0e319
alan6333 <alan6333.business@gmail.com>
2022-12-21 01:07:02 -0600

    remove .next folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75ca351041fbb8a6"; }
+/******/ 		__webpack_require__.h = function() { return "1df79fc3276b1087"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
