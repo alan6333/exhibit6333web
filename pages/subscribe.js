3e9a4fa6e48e003a0459b76bbcdea8a008615eab
alan6333 <alan6333.business@gmail.com>
2022-12-27 00:02:14 -0600

    fix width2

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -5,7 +5,7 @@ import Navbar from '../components/Navbar'
 
 export default () => {
     return (
-        <div class='-z-0 h-screen bg-hero bg-repeat bg-local bg-20%'>
+        <div class='-z-0 h-screen w-fit min-w-full bg-hero bg-repeat bg-local bg-20%'>
             <div className=' py-7 grid place-content-center'>
                 <div className="text-5xl UrbanistFont.localFont transition-all hover:text-6xl hover:duration-75">Subscribe</div>
             </div>
