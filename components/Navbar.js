b6f8543d79f62d756d094d635cf2a60bb080a546
alan6333 <alan6333.business@gmail.com>
2022-12-28 14:21:39 -0600

    navbar state hooks

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,24 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
+
 
 export default () => {
+    {/* Get the current route */}
+    const router = useRouter();
+    const currentRoute = router.pathname;
+
     return (
+
       <div className='z-10 bg-stone-500 py-6 text-center'>
         <div>
             <ul>
-                <li className=' px-10 inline text-3xl UrbanistFont.localFont text-neutral-400 hover:text-gray-300 hover:font-semibold'>
+                <li className={currentRoute === '/' ? 'px-10 inline text-3xl text-gray-300 font-semibold UrbanistFont.localFont' : 'px-10 inline text-3xl UrbanistFont.localFont text-neutral-400 hover:text-neutral-600 hover:font-semibold'}>
                     <Link href='/'>Exhibition</Link>
                 </li>
-                <li className='px-10 inline text-3xl UrbanistFont.localFont text-neutral-400 hover:text-gray-300 hover:font-semibold'>
+                <li className={currentRoute === '/artist' ? 'px-10 inline text-3xl text-gray-300 font-semibold UrbanistFont.localFont' : 'px-10 inline text-3xl UrbanistFont.localFont text-neutral-400 hover:text-neutral-600 hover:font-semibold'}>
                     <Link href='/artist'>Artist6333</Link>
                 </li>
-                <li className='px-10 inline text-3xl UrbanistFont.localFont text-neutral-400 hover:text-gray-300 hover:font-semibold'>
+                <li className={currentRoute === '/subscribe' ? 'px-10 inline text-3xl text-gray-300 font-semibold UrbanistFont.localFont' : 'px-10 inline text-3xl UrbanistFont.localFont text-neutral-400 hover:text-neutral-600 hover:font-semibold'}>
                     <Link href='/subscribe'>Subscribe</Link>
                 </li>
             </ul>
