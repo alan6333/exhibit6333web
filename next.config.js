431b1e770e2af403e4594dcbe1f8d43dceaf2b62
alan6333 <alan6333.business@gmail.com>
2022-12-27 23:27:04 -0600

    pull from s3 instead

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// next.config.js
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'storage6333.s3.us-east-2.amazonaws.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
